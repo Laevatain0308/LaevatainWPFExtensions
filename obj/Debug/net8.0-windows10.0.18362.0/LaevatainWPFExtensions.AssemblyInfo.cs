f5248cea336189dc21b0db4357e212fa9ac27c72
Laevatain <1532474791@qq.com>
2026-01-04 20:46:39 +0800

    Create Repository

--- obj/Debug/net8.0-windows10.0.18362.0/LaevatainWPFExtensions.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.18362.0/LaevatainWPFExtensions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaevatainWPFExtensions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bdcca1f930eaca0479742e5a794b3b5f3fe8d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaevatainWPFExtensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaevatainWPFExtensions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
